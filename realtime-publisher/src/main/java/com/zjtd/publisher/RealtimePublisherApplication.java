416518d12d881f92913cd9da19e57d7c77c8745e
admin <admin@admindeMacBook-Pro-4.local>
2021-07-08 17:55:31 +0800

    数据接口模块

---
+++ realtime-publisher/src/main/java/com/zjtd/publisher/RealtimePublisherApplication.java
@@ -0,0 +1,13 @@
+package com.zjtd.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimePublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimePublisherApplication.class, args);
+    }
+
+}
---
+++ realtime-publisher/src/test/java/com/zjtd/publisher/RealtimePublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjtd.publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimePublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
